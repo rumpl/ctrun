0cc1dd9d5e78e98b084942158be4deee4cacd85b
Djordje Lukic <djordje.lukic@docker.com>
2020-08-25 18:16:52 +0200

    Output untarred image

--- go.mod
+++ go.mod
@@ -4,22 +4,23 @@ go 1.14
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
-	github.com/containerd/console v0.0.0-20191219165238-8375c3424e4d
+	github.com/containerd/console v1.0.0
 	github.com/docker/distribution v2.7.1-0.20190205005809-0d3efadf0154+incompatible
 	github.com/docker/docker v1.14.0-0.20190319215453-e7b5f7dbe98c
 	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/gorilla/mux v1.7.2
+	github.com/gorilla/mux v1.8.0
 	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/moby/buildkit v0.7.2
+	github.com/moby/moby v1.13.1
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/opencontainers/selinux v1.3.3 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.4.2
+	github.com/sirupsen/logrus v1.6.0
 	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
--- server.go
+++ server.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"archive/tar"
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/docker/distribution/reference"
+	"github.com/docker/docker/api/types"
 	dockerclient "github.com/docker/docker/client"
 	"github.com/gorilla/mux"
 	"github.com/moby/buildkit/client"
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/moby/pkg/stdcopy"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
@@ -23,6 +29,7 @@ func server(clix *cli.Context) error {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/v2/{name:"+reference.NameRegexp.String()+"}/manifests/{reference}", manifests)
+	router.HandleFunc("/v2/{name:"+reference.NameRegexp.String()+"}/blobs/{reference}", manifests)
 
 	srv := &http.Server{
 		Handler:      router,
@@ -34,6 +41,24 @@ func server(clix *cli.Context) error {
 	return srv.ListenAndServe()
 }
 
+func demuxConn(c net.Conn) net.Conn {
+	pr, pw := io.Pipe()
+	go stdcopy.StdCopy(pw, os.Stderr, c)
+	return &demux{
+		Conn:   c,
+		Reader: pr,
+	}
+}
+
+type demux struct {
+	net.Conn
+	io.Reader
+}
+
+func (d *demux) Read(dt []byte) (int, error) {
+	return d.Reader.Read(dt)
+}
+
 func manifests(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
@@ -42,8 +67,25 @@ func manifests(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logrus.Fatal(err)
 	}
+	dc.NegotiateAPIVersion(context.Background())
+	response, err := dc.ContainerExecCreate(context.Background(), "buildx_buildkit_objective_noyce0", types.ExecConfig{
+		Cmd:          []string{"buildctl", "dial-stdio"},
+		AttachStdin:  true,
+		AttachStdout: true,
+		AttachStderr: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	resp, err := dc.ContainerExecAttach(context.Background(), response.ID, types.ExecStartCheck{})
+	if err != nil {
+		panic(err)
+	}
+
+	conn := demuxConn(resp.Conn)
+
 	c, err := client.New(context.Background(), "", client.WithDialer(func(string, time.Duration) (net.Conn, error) {
-		return dc.DialHijack(context.Background(), "/grpc", "h2c", nil)
+		return conn, nil
 	}))
 	if err != nil {
 		logrus.Fatal(err)
@@ -53,11 +95,17 @@ func manifests(w http.ResponseWriter, r *http.Request) {
 
 	solveOpt := client.SolveOpt{
 		Frontend: "dockerfile.v0",
-		Exports:  []client.ExportEntry{{Type: "moby", Attrs: map[string]string{}}},
+		FrontendAttrs: map[string]string{
+			"context":  "git://" + vars["name"],
+			"filename": "Dockerfile",
+		},
+		Exports: []client.ExportEntry{
+			{
+				Type:   "oci",
+				Output: wrapWriteCloser(),
+			},
+		},
 	}
-	solveOpt.FrontendAttrs = map[string]string{}
-	solveOpt.FrontendAttrs["context"] = "git://" + vars["name"]
-	solveOpt.FrontendAttrs["filename"] = "Dockerfile"
 
 	eg, ctx := errgroup.WithContext(context.Background())
 
@@ -83,5 +131,48 @@ func manifests(w http.ResponseWriter, r *http.Request) {
 
 	if err = eg.Wait(); err != nil {
 		logrus.Fatal(err)
+	}
+}
+
+func wrapWriteCloser() func(map[string]string) (io.WriteCloser, error) {
+	pr, pw := io.Pipe()
+	// TODO: check errors
+	go func() {
+		tr := tar.NewReader(pr)
+
+		for {
+			header, err := tr.Next()
+			switch {
+			case err == io.EOF:
+				return
+			case err != nil:
+				return
+			case header == nil:
+				return
+			}
+
+			target := filepath.Join("/tmp/that", header.Name)
+			switch header.Typeflag {
+			case tar.TypeDir:
+				if _, err := os.Stat(target); err != nil {
+					if err := os.MkdirAll(target, 0755); err != nil {
+						return
+					}
+				}
+			case tar.TypeReg:
+				f, err := os.OpenFile(target, os.O_APPEND|os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+				if err != nil {
+					return
+				}
+				if _, err := io.Copy(f, tr); err != nil {
+					return
+				}
+				f.Close()
+			}
+		}
+	}()
+
+	return func(d map[string]string) (io.WriteCloser, error) {
+		return pw, nil
 	}
 }
